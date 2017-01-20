cd8480e06b217a2ebb5a99103f966fa44f055d52
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-20 15:18:09 -0800

    Add class for category requests

---
+++ category.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace KVSun\KVSAPI;
+
+final class Category extends Abstracts\Content
+{
+	const TYPE = 'category';
+	const DEFAULTS = [
+		'articles' => [],
+	];
+
+	private $_start = 0;
+	private $_end   = 7;
+
+	public function __construct(\PDO $pdo, $url = null, $start = 0, $end = 7)
+	{
+		$this->_start = $start;
+		$this->_end = $end;
+		parent::__construct($pdo, $url);
+	}
+
+	protected function _getSQL()
+	{
+		$sql = 'SELECT
+			`posts`.`title`,
+			`posts`.`author`,
+			`posts`.`img`,
+			`posts`.`url`,
+			`posts`.`posted`,
+			`categories`.`url-name` AS `catURL`,
+			`categories`.`parent`
+		FROM `posts`
+		JOIN `categories` ON `categories`.`id` = `posts`.`cat-id`
+		WHERE `categories`.`name` = :name
+		LIMIT %d, %d;';
+
+		return sprintf($sql, $this->_start, $this->_end);
+	}
+
+	protected function _setData(\PDOStatement $stm)
+	{
+		$path = $this->_parsePath();
+		$cat = $path[0];
+		$stm->bindParam(':name', $cat);
+		$stm->execute();
+		$results = $stm->fetchAll(\PDO::FETCH_CLASS);
+
+		$this->_set('category', $cat);
+		$this->_set('articles', $results);
+	}
+}
