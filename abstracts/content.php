b444db650d0e59c4727a8f3b82fd11927201418a
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-20 09:30:25 -0800

    Add initial classes

---
+++ abstracts/content.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace KVSun\KVSAPI\Abstracts;
+
+use \shgysk8zer0\Core_API as API;
+use \shgysk8zer0\Core as Core;
+
+abstract class Content implements \JsonSerializable
+{
+	const MAGIC_PROPERTY = '_data';
+
+	private $_data = array();
+	protected $_url;
+
+	final public function __construct(\PDO $pdo, $url = null)
+	{
+		$this->{self::MAGIC_PROPERTY} = $this::DEFAULTS;
+
+		$this->_parseURL($url);
+		if ($this->_url['path'] !== '/') {
+			$stm = $pdo->prepare($this->_getSQL());
+			$this->_setData($stm);
+		} else {
+			throw new \Exception('Attempting to load Article for home page');
+		}
+	}
+
+	final public function __isset($prop)
+	{
+		return isset($this->{self::MAGIC_PROPERTY}[$prop]);
+	}
+
+	final public function __get($prop)
+	{
+		if ($this->__isset($prop)) {
+			return $this->{self::MAGIC_PROPERTY}[$prop];
+		}
+	}
+
+	final public function jsonSerialize()
+	{
+		return [
+			'type' => $this::TYPE,
+			'url' => $this->_url,
+			'data' => $this->{self::MAGIC_PROPERTY}
+		];
+	}
+
+	final public function __debugInfo()
+	{
+		return $this->{self::MAGIC_PROPERTY};
+	}
+
+	final protected function _parsePath()
+	{
+		$path = explode('/', trim($this->_url['path'], '/'));
+		$path = array_filter($path);
+		return $path;
+	}
+
+	final private function _parseURL($url = null)
+	{
+		if (is_string($url)) {
+			$url = parse_url($url);
+		} else {
+			$url = [];
+		}
+
+		$this->_url = array_merge([
+			'scheme' => $_SERVER['REQUEST_SCHEME'],
+			'host' => $_SERVER['HTTP_HOST'],
+			'path' => $_SERVER['SCRIPT_NAME'],
+		], $url);
+	}
+
+	final protected function _set($prop, $value)
+	{
+		$this->{self::MAGIC_PROPERTY}[$prop] = $value;
+		return $this;
+	}
+
+	abstract protected function _getSQL();
+
+	abstract protected function _setData(\PDOStatement $stm);
+}
---
+++ article.php
@@ -0,0 +1,89 @@
+<?php
+namespace KVSun\KVSAPI;
+
+final class Article extends Abstracts\Content
+{
+	const TYPE = 'article';
+	const DEFAULTS = [
+		'id'          => null,
+		'category'    => null,
+		'title'       => null,
+		'author'      => null,
+		'content'     => null,
+		'posted'      => null,
+		'updated'     => null,
+		'draft'       => true,
+		'url'         => [],
+		'img'         => null,
+		'posted_by'   => null,
+		'keywords'    => [],
+		'description' => null,
+		'is_free'     => true,
+	];
+
+	protected function _setData(\PDOStatement $stm)
+	{
+		$path = $this->_parsePath();
+
+		if (count($path) === 2) {
+			list($category, $url) = $path;
+			$stm->bindParam(':url', $url);
+			$stm->bindParam(':category', $category);
+			$stm->execute();
+			$results = $stm->fetchObject();
+			if (isset(
+				$results->id,
+				$results->category,
+				$results->title,
+				$results->author,
+				$results->content,
+				$results->posted
+			)) {
+				if (isset($results->keywords)) {
+					$keywords = explode(',', $results->keywords);
+					$keywords = array_filter($keywords);
+					$keywords = array_map('trim', $keywords);
+				} else {
+					$keywords = [];
+				}
+
+				$this->_set('id', intval($results->id));
+				$this->_set('category', $results->category);
+				$this->_set('title', $results->title);
+				$this->_set('author', $results->author);
+				$this->_set('content', $results->content);
+				$this->_set('posted', $results->posted);
+				$this->_set('updated', $results->updated);
+				$this->_set('draft', $results->draft === '1');
+				$this->_set('url', $results->url);
+				$this->_set('img', $results->img);
+				$this->_set('posted_by', $results->posted_by);
+				$this->_set('keywords', $keywords);
+				$this->_set('description', $results->description);
+				$this->_set('is_free', !isset($results->is_free) or $results->is_free === '1');
+			}
+		}
+	}
+
+	protected function _getSQL()
+	{
+		return 'SELECT `posts`.`id`,
+			`categories`.`name` AS `category`,
+			`posts`.`title`,
+			`posts`.`author`,
+			`posts`.`content`,
+			`posts`.`posted`,
+			`posts`.`updated`,
+			`posts`.`draft`,
+			`posts`.`url`,
+			`posts`.`img`,
+			`user_data`.`name` AS `posted_by`,
+			`posts`.`keywords`,
+			`posts`.`description`
+		FROM `posts`
+		JOIN `user_data` ON `posts`.`posted_by` = `user_data`.`id`
+		JOIN `categories` ON `categories`.`id` = `posts`.`cat-id`
+		WHERE `posts`.`url` = :url AND `categories`.`name` = :category
+		LIMIT 1;';
+	}
+}
