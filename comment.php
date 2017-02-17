d45ed0472c93d6daa43ab1e6dd519ce1d43dc949
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-17 14:35:41 -0800

    Update documentation

--- comment.php
+++ comment.php
@@ -13,24 +13,53 @@ final class Comment extends \ArrayObject implements \JsonSerializable
 		'email',
 	];
 
-	public function __construct(\stdClass $comment_data)
+	/**
+	 * Create a comment instance from comment data
+	 * @param stdClass $comment_data Data from comments table
+	 * @param boolean  $hash_email   Whether or not to convert email to MD5 hash
+	 */
+	public function __construct(\stdClass $comment_data, Bool $hash_email = true)
 	{
 		if (!static::isValid($comment_data)) {
 			throw new \InvalidArgumentException('Comment does not contain all required properties.');
+		} elseif ($hash_email) {
+			$comment_data->email = md5(strtolower($comment_data->email));
 		}
 		parent::__construct(get_object_vars($comment_data), self::ARRAY_AS_PROPS);
 	}
 
+	/**
+	 * Method used when class instance is used as a string
+	 * @return string Return comment text
+	 */
+	public function __toString(): String
+	{
+		return $this->text;
+	}
+
+	/**
+	 * Method used when class instance is passed to `json_encode`
+	 * @return Array Comment data as array
+	 */
 	public function jsonSerialize(): Array
 	{
 		return $this->getArrayCopy();
 	}
 
+	/**
+	 * Method used when class instance passed to debugging function, such as `var_dump`
+	 * @return Array Comment data as array
+	 */
 	public function __debugInfo(): Array
 	{
 		return $this->getArrayCopy();
 	}
 
+	/**
+	 * Checks if $omment contains all requried fields
+	 * @param  stdClass $comment Comment data to check
+	 * @return Bool              Whether or not all fields exist
+	 */
 	public static function isValid(\stdClass $comment): Bool
 	{
 		$valid = true;
--- comments.php
+++ comments.php
@@ -4,6 +4,10 @@ namespace KVSun\KVSAPI;
 
 final class Comments extends \SplObjectStorage implements \JsonSerializable
 {
+	/**
+	 * Create a Comments instance from a list of comments
+	 * @param stdClass $comments $comment1, $comment2, ...
+	 */
 	public function __construct(\stdClass ...$comments)
 	{
 		foreach ($comments as $comment) {
@@ -11,16 +15,28 @@ final class Comments extends \SplObjectStorage implements \JsonSerializable
 		}
 	}
 
+	/**
+	 * Method used when class instance passed to `json_encode`
+	 * @return Array Array of Comments
+	 */
 	public function jsonSerialize(): Array
 	{
 		return $this->_asArray();
 	}
 
+	/**
+	 * Method used when class instance passed to debugging function, such as `var_dump`
+	 * @return Array Array of Comments
+	 */
 	public function __debugInfo(): Array
 	{
 		return $this->_asArray();
 	}
 
+	/**
+	 * Private method for converting comment storage to an array
+	 * @return Array Array of Comments
+	 */
 	private function _asArray(): Array
 	{
 		$items = [];
@@ -30,23 +46,37 @@ final class Comments extends \SplObjectStorage implements \JsonSerializable
 		return $items;
 	}
 
+	/**
+	 * Static method to get comments for an article / post
+	 * @param  PDO     $pdo             Instance of database connection
+	 * @param  Int     $post_id         Post ID
+	 * @param  Int     $cat_id          Category ID
+	 * @param  boolean $filter_approved Filter out comments that are not approved
+	 * @param  Int     $start           Starting index
+	 * @param  Int     $end             Ending index
+	 * @param  Bool    $hash_email      Convert email to MD5 hash, I.E. for Gravatar
+	 * @return self                     A new instance of Comments filled with comments on post
+	 */
 	public static function getComments(
 		\PDO $pdo,
 		Int  $post_id,
 		Int  $cat_id,
-		Int  $start = 0,
-		Int  $end   = PHP_INT_MAX
+		Bool $filter_approved = true,
+		Int  $start           = 0,
+		Int  $end             = PHP_INT_MAX,
+		Bool $hash_email      = true
 	): self
 	{
 		try {
 			$stm = $pdo->prepare(
 				"SELECT
-				`post_comments`.`created`,
-				`post_comments`.`text`,
-				`post_comments`.`id` AS `commentID`,
-				`user_data`.`name`,
-				`users`.`username`,
-				`users`.`email`
+					`post_comments`.`created`,
+					`post_comments`.`text`,
+					`post_comments`.`id` AS `commentID`,
+					`post_comments`.`approved`,
+					`user_data`.`name`,
+					`users`.`username`,
+					`users`.`email`
 				FROM `post_comments`
 				JOIN `posts` ON `posts`.`id` = `post_comments`.`postID`
 					AND `posts`.`cat-id` = `post_comments`.`catID`
@@ -54,7 +84,6 @@ final class Comments extends \SplObjectStorage implements \JsonSerializable
 				JOIN `user_data` ON `user_data`.`id` = `post_comments`.`userID`
 				WHERE `posts`.`id` = :post
 					AND `posts`.`cat-id` = :cat
-					AND `post_comments`.`approved` = 1
 				ORDER BY `post_comments`.`created` DESC
 				LIMIT $start, $end;"
 			);
@@ -64,16 +93,25 @@ final class Comments extends \SplObjectStorage implements \JsonSerializable
 			if (intval($stm->errorCode()) !== 0) {
 				throw new \Exception('SQL Error: '. join(PHP_EOL, $stm->errorInfo()));
 			}
-			$comments = $stm->getResults();
-			// Convert user email to an MD5, useful for Gravatar
-			array_walk($comments, function(\stdClass &$comment)
-			{
-				$comment->email = md5(strtolower($comment->email));
-			});
+			$comments = $stm->fetchAll(\PDO::FETCH_CLASS);
+			\shgysk8zer0\Core\Console::log($comments);
+			if ($filter_approved) {
+				$comments = array_filter($comments, __CLASS__ . '::_isApproved');
+			}
 		} catch (\Throwable $e) {
 			trigger_error($e->getMessage());
 		} finally {
-			return new self(...$comments ?? []);
+			return new self(...$comments ?? null);
 		}
+	}
+
+	/**
+	 * Private method for filtering comments by `approved` column in table
+	 * @param  StdClass $comment Comment data
+	 * @return Bool              Whether or not the comment has been approved
+	 */
+	private static function _isApproved(\StdClass $comment): Bool
+	{
+		return isset($comment->approved) and $comment->approved == 1;
 	}
 }
